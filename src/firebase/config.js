f99b61238a62be39a7974e84e6c627ca0bb6c4db
WouS95 <wouterspaan@msn.com>
2021-11-08 12:47:26 +0100

    install firebase essentials

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyBfez8ELB9LQ2DcUx9wIsoGEirBcQYKw4s",
+    authDomain: "brgr-ce55d.firebaseapp.com",
+    projectId: "brgr-ce55d",
+    storageBucket: "brgr-ce55d.appspot.com",
+    messagingSenderId: "192325240024",
+    appId: "1:192325240024:web:eb8960af96ff5a771183ad",
+    measurementId: "G-XPFN1HPNHW"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init firestore service
+const projectFirestore = firebase.firestore();
+export {
+    projectFirestore
+}
