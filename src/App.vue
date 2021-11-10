c7bd078140b16e31bc6f3b669a44046c72d148e7
neomie <noemiandriesse@gmail.com>
2021-11-10 11:03:10 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,32 @@
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+body,
+html {
+  /* background: #1b5e20; */
+  background: #f1fdf2;
+  margin: 0;
+  font-size: 18px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Poppins, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
+}
+button {
+  background-color: #d32f2f;
+  border: none;
+  color: white;
+  padding: 1em 2.5em;
+  border-radius: 2em;
+  cursor: pointer;
+  font-weight: 500;
 }
 
 #nav {
